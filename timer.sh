2a042f38b2b1c667fb0335765f871d8e1fbda50c
shivgarg5676 <shivgarg5676@hotmail.com>
2015-07-22 00:04:09 +0530

    Create timer.sh

---
+++ timer.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+time=`date | cut -d " " -f4 | cut -d ":" -f1,2`
+minute=`date | cut -d " " -f4 | cut -d ":" -f2`
+if (( minute%$1==0 )) 
+then
+notify-send $time
+espeak -v female3 "the time is $time" 
+fi
