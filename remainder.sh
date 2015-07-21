4da7aee9b00304b122c89062e0617934f4f733be
shivgarg5676 <shivgarg5676@hotmail.com>
2015-07-22 00:03:27 +0530

    Create remainder.sh

---
+++ remainder.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+day=`date | cut -d " " -f3`
+month=`date | cut -d " " -f2`
+yearr=`date | cut -d " " -f6`
+hour_current=`date | cut -d " " -f4 | cut -d ":" -f1`
+minute_current=`date | cut -d " " -f4 | cut -d ":" -f2`
+source=/tmp/.remainder/
+filename=$source$day-$month-$yearr
+if [ -f $filename ]
+then 
+IFS="	"
+while read hour minute title message 
+do
+if [[ $hour -eq $hour_current && $minute -eq $minute_current ]]
+then
+notify-send $title $message
+espeak -v female3 $title
+fi
+done <$filename 
+fi
